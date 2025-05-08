0cc550408691b701c9702656e6bbd4920c9e6ef2
Aditya Sharma <adi01012004@gmail.com>
2025-05-09 00:03:18 +0530

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,34 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import { visualizer } from 'rollup-plugin-visualizer'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//     visualizer({
+//       open: true,
+//       gzipSize: true,
+//       brotliSize: true,
+//       filename: 'dist/bundle-analysis.html',
+//     }),
+//   ],
+//   base: './',
+// }
+// )
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { visualizer } from 'rollup-plugin-visualizer'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,5 +43,13 @@ export default defineConfig({
     }),
   ],
   base: './',
-}
-)
+  build: {
+    outDir: '../backend/client', // ⬅️ Output path to match Express static folder
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
