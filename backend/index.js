67a40425bb7463ba1ffc6516704940733918124e
Aditya Sharma <adi01012004@gmail.com>
2025-05-12 15:39:17 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -143,10 +143,10 @@ app.use("/pg", pgRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, './client')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
 // Database connection
