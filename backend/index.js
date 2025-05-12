fb096d4f0dfefea16f5234e1f19a2c55c465b513
Aditya Sharma <adi01012004@gmail.com>
2025-05-12 15:46:10 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -146,7 +146,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
 // Database connection
