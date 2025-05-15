f556a39506ef0ae653c4f5983c692511f297ef5e
Aditya Sharma <adi01012004@gmail.com>
2025-05-15 16:41:00 +0530

    static

--- backend/index.js
+++ backend/index.js
@@ -144,13 +144,13 @@ console.log(import.meta.url)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, './dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('dist'))
 
 app.get('*', (req, res) => {
- res.sendFile(path.join(__dirname, './dist','index.html'));
+ res.sendFile(path.join(__dirname, 'dist','index.html'));
 });
 
-app.use(express.static('dist'))
 
 // Database connection
 mongoose.connect(
